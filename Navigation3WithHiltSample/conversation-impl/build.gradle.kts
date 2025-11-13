8d1f0ef557583a98eb219c53a1882dc509598930
Sungyong An <sungyong.an@webtoonscorp.com>
2025-11-14 00:40:55 +0900

    Navigation3 + Hilt

---
+++ Navigation3WithHiltSample/conversation-impl/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.example.conversation.impl"
+    defaultConfig {
+        minSdk = 24
+        compileSdk = 36
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":profile-api"))
+    implementation(project(":conversation-api"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.navigation3.runtime)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
---
+++ Navigation3WithHiltSample/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Navigation3WithHiltSample"
+include(":app")
+include(":common")
+include(":conversation-api")
+include(":conversation-impl")
+include(":profile-api")
+include(":profile-impl")
